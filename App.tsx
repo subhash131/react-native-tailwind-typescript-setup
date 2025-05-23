ecc740d60ff789b84ca0430be4b7d311457ff09a
subhash131 <subhashnayak131@gmail.com>
2025-05-23 07:56:03 +0530

    react-native tailwind setup

--- App.tsx
+++ App.tsx
@@ -1,20 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import Navigation from "./app/navigation";
+import "./global.css";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider>
+      <Navigation />
+    </SafeAreaProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ app/screens/auth/RegisterScreen.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+export default function RegisterScreen() {
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <Text>RegisterScreen</Text>
+    </SafeAreaView>
+  );
+}
